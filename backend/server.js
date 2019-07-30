e9741b99b1dc429475d56384cfdef46d223f9a75
Cece Claridge <cece.claridge@gmail.com>
2019-07-30 10:04:23 -0400

    created react components

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const port = process.env.PORT || 5000;
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
-
 app.use(cors());
 app.use(express.json());
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/NavBar"
+import ExercisesList from "./components/ExerciseList";
+import EditExercise from "./components/EditExercise";
+import CreateExercise from "./components/CreateExercise";
+import CreateUser from "./components/CreateUser";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div className="container">
+     Hello World
+   </div>
+ );
 }
 
 export default App;
